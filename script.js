6b8efb1e2d91f6a48ee130392506b8115972d13d
KJWoo99 <167010073+KJWoo99@users.noreply.github.com>
2024-06-27 14:35:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,22 +6,12 @@ const recognition = new SpeechRecognition();
 const resultContainer = document.getElementById('resultContainer');
 let finalTranscript = '';
 
-// 모달 요소 선택
-const modal = document.getElementById('modal');
-const confirmButton = document.getElementById('confirmButton');
-const cancelButton = document.getElementById('cancelButton');
-
 // 시작/멈춤 버튼 선택
 const startStopButton = document.getElementById('startStopButton');
 startStopButton.addEventListener('click', toggleRecognition);
 
-// 결과 처리를 위한 플래그
-let processResults = true;
-
 // 음성 인식 결과 처리
 recognition.onresult = (event) => {
-    if (!processResults) return;
-
     let interimTranscript = '';
     for (let i = event.resultIndex; i < event.results.length; i++) {
         const transcript = event.results[i][0].transcript;
@@ -31,56 +21,16 @@ recognition.onresult = (event) => {
             interimTranscript += transcript;
         }
     }
-
-    // Reset silence timer on receiving a result
     resetSilenceTimer();
 };
-
-// 모달창 보여주는 함수
-function showModal() {
-    pauseRecognition();
-    modal.style.display = 'block';
-    processResults = false; // 결과 처리 중지
-}
-
-// 모달창 숨기는 함수
-function hideModal() {
-    modal.style.display = 'none';
-    processResults = true; // 결과 처리 다시 시작
-    if (!startStopButton.classList.contains('active')) {
-        startRecognition();
-    }
-}
-
-// 모달 버튼 클릭 이벤트 핸들러 - 예 버튼
-confirmButton.addEventListener('click', () => {
-    hideModal();
-    window.location.href = 'my.html';
-});
-
-// 모달 버튼 클릭 이벤트 핸들러 - 아니요 버튼
-cancelButton.addEventListener('click', () => {
-    hideModal();
-    startRecognition();
-});
 
 // 음성 인식 시작/멈춤 토글 함수
 function toggleRecognition() {
     if (startStopButton.classList.contains('active')) {
-        showModal();
+        stopRecognition();
     } else {
-        requestMicrophoneAccess().then(startRecognition).catch(handleMicrophoneAccessError);
+        startRecognition();
     }
-}
-
-// 음성 인식 일시정지 함수
-function pauseRecognition() {
-    recognition.stop();
-}
-
-// 음성 인식 재시작 함수
-function resumeRecognition() {
-    recognition.start();
 }
 
 // 음성 인식 시작 함수
@@ -89,7 +39,6 @@ function startRecognition() {
     recognition.start();
     console.log('음성 인식 시작');
     startStopButton.classList.add('active');
-
     resetSilenceTimer();
 }
 
@@ -155,4 +104,3 @@ function handleMicrophoneAccessError(err) {
     console.error('마이크 권한을 받을 수 없습니다:', err);
     alert('마이크 권한이 필요합니다. 브라우저 설정에서 마이크 권한을 허용해주세요.');
 }
-
